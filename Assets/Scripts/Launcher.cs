2990b18b65e066f7125f81eb73139d92582b539a
abdalrahmanYaseen <abdalrahman.yaseen@gmail.com>
2020-10-26 00:52:53 +0100

    Connection to server established. Creating and adding a room functionality established. Sample stypid first scene was built

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public GameObject ConnectedScreen;
+    public GameObject DisconnectedScreen;
+    public string Error;
+    public void  OnClick_ConnectBtn()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        
+    }
+
+    public override void OnJoinedLobby()
+    {
+        if (DisconnectedScreen.activeSelf)
+            DisconnectedScreen.SetActive(false);
+        ConnectedScreen.SetActive(true);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectedScreen.SetActive(true);
+        Error = cause.ToString();
+    }
+}
---
+++ Assets/Scripts/UI_handler.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_handler : MonoBehaviourPunCallbacks
+{
+
+    public InputField CreateRoomTF;
+    public InputField JoinRoomTF;
+
+    public void OnClick_JoinRoom() {
+
+        PhotonNetwork.JoinRoom(JoinRoomTF.text, null);
+
+    
+    }
+    public void OnClick_CreateRoom() {
+
+        PhotonNetwork.CreateRoom(CreateRoomTF.text, new Photon.Realtime.RoomOptions { MaxPlayers = 4 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("JOINED");
+        PhotonNetwork.LoadLevel(1);
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        print("FAILED TO JOIN ROOM");
+    }
+}
