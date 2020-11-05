4d28c4321e4934716c85c6549cbda8c1a2c2cf7d
abdelrahmanYaseen <abdalrahman.yaseen@gmail.com>
2020-11-05 01:26:23 +0100

    Milk can now be spawned

--- Assets/Scripts/Gameplay/EnvironmentManager.cs
+++ Assets/Scripts/Gameplay/EnvironmentManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.SocialPlatforms;
 
 
 
@@ -13,13 +14,21 @@ it spawns the cat above a random balcony.
 (Doesn't check if there's a cat already there)
  
  */
-public class EnvironmentManager : MonoBehaviour
+public class EnvironmentManager : MonoBehaviourPun
 {
     public GameObject catPrefab;
+    public GameObject milkPrefab;
+
+    [SerializeField]  private int _maxMilkSpawned = 10;
+    [SerializeField]  private float _timeBetweenMilkSpawns = 5f;
 
     void Start()
     {
         CatSpawn();
+        if (PhotonNetwork.IsMasterClient) //YASEEN: If I'm the owner of the scene ? 
+            StartCoroutine(spawnMilk(_maxMilkSpawned));
+        else
+            Debug.Log("NOT MASTER");
     }
 
     private void CatSpawn()
@@ -31,5 +40,18 @@ public class EnvironmentManager : MonoBehaviour
     }
 
     // Update is called once per frame
+ 
+    private IEnumerator spawnMilk(int maxNumber)
+    {
+        WaitForSeconds wait = new WaitForSeconds(_timeBetweenMilkSpawns);
+        foreach (int value in System.Linq.Enumerable.Range(1, maxNumber))
+        {
+
+            var spawnPosition = Utility.getRandomSpawnLocation();
+            PhotonNetwork.Instantiate(milkPrefab.name, spawnPosition, milkPrefab.transform.rotation); //spawn milk :)
+            Debug.Log("MILK COOUUNT - " + value);
+            yield return wait;
+        }
+    }
 
 }
