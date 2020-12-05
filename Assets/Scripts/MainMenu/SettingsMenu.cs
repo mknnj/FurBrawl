81f3d0ecf174b8a3f8a8c2711acfa0282ae91d61
michele scandelli <miki199714@gmail.com>
2020-12-05 15:44:09 +0100

    toggle full screen setting

---
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void OnToggle_FullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+}
