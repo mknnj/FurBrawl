4325ddd4ca2e469e358fedcb7780ffdd8bf721f2
michele scandelli <miki199714@gmail.com>
2021-01-03 22:34:59 +0100

    fix shader bug in victory skin

--- Assets/Scripts/MainMenu/VictoryMenu.cs
+++ Assets/Scripts/MainMenu/VictoryMenu.cs
@@ -11,6 +11,7 @@ public class VictoryMenu : MonoBehaviour
     [SerializeField] private Image _winnerImage;
     [SerializeField] private Text _winnerText;
     [SerializeField] private Sprite[] catSkinsList;
+    [SerializeField] private Material[] catMaterialList;
     [SerializeField] private ScreenManager screenManager;
     void OnEnable()
     {
@@ -19,8 +20,17 @@ public class VictoryMenu : MonoBehaviour
         if (winner != null)
         {
             int _skin = (int) winner.CustomProperties["SkinID"];
-
-            _winnerImage.sprite = catSkinsList[_skin];
+            CatSkin targetSkin = CatSkins.catSkinsList[_skin];
+            _winnerImage.sprite = catSkinsList[targetSkin.baseSkinID];
+            Material mat = new Material(catMaterialList[targetSkin.baseSkinID]);
+            mat.SetColor("_SkinABC",
+                new Color(targetSkin.skinColor.r / 255, targetSkin.skinColor.g / 255, targetSkin.skinColor.b / 255));
+            mat.SetColor("_DotsABC",
+                new Color(targetSkin.dotsColor.r / 255, targetSkin.dotsColor.g / 255, targetSkin.dotsColor.b / 255));
+            mat.SetColor("_DetailsABC",
+                new Color(targetSkin.detailsColor.r / 255, targetSkin.detailsColor.g / 255,
+                    targetSkin.detailsColor.b / 255));
+            _winnerImage.material = mat;
             _winnerText.text = winner.NickName + " Wins!!";
         }
     }
